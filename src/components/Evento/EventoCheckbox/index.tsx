ac3258197818a0b0477312a6400b0804c33cd9bc
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-11-05 16:27:27 -0300

    refact(componentes): formatando arquivos

--- src/components/Evento/EventoCheckbox/index.tsx
+++ src/components/Evento/EventoCheckbox/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { IEvento } from '../../../interfaces/IEvento';
+import React from "react";
+import { IEvento } from "../../../interfaces/IEvento";
 
-const EventoCheckbox: React.FC<{ evento: IEvento, aoAlterarStatus: (id: number) => void }> = ({ evento, aoAlterarStatus }) => {
-  
+const EventoCheckbox: React.FC<{
+  evento: IEvento;
+  aoAlterarStatus: (id: number) => void;
+}> = ({ evento, aoAlterarStatus }) => {
   const estilos = [
-    'far',
-    'fa-2x',
-    evento.completo ? 'fa-check-square' : 'fa-square'
-  ]
+    "far",
+    "fa-2x",
+    evento.completo ? "fa-check-square" : "fa-square",
+  ];
 
-  return (<i className={estilos.join(' ')} onClick={() => aoAlterarStatus(evento.id!)}></i>)
-}
+  return (
+    <i
+      className={estilos.join(" ")}
+      onClick={() => aoAlterarStatus(evento.id!)}
+    ></i>
+  );
+};
 
-export default EventoCheckbox
+export default EventoCheckbox;
